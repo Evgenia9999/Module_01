2a8f58f99603a90af2d71be6107e164bc620f3a0
Evgenia Avramenko <hapjane@yandex.ru>
2023-07-01 17:50:03 +0500

    Создан урок 2

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const nameItem = 'Phone';
+let numItem = 5;
+const category = 'Gadgets';
+let price = 10000;
+
+console.log(nameItem);
+console.log(`общая сумма товара: ${numItem * price}`);
+
+{
+    const nameItem1 = 'Apple';
+    let numItem1 = 3;
+    const category1 = 'Fruits';
+    let price1 = 10;
+    
+    console.log(nameItem1);
+    console.log(`общая сумма товара: ${numItem1 * price1}`);
+}
