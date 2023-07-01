076d7b042be8c3c4fb710fa0b9afc974104fad41
Evgenia Avramenko <hapjane@yandex.ru>
2023-07-01 18:50:03 +0500

    Сделала задания к теме 3

---
+++ js/einstein.js
@@ -0,0 +1,7 @@
+const myWeight = +prompt('Введите ваш вес');
+const speedLight = 3e8;
+const energy = myWeight * (3e8)^2;
+console.log('energy: ', energy);
+
+
+
--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@ let numItem = 5;
 const category = 'Gadgets';
 let price = 10000;
 
-console.log(nameItem);
-console.log(`общая сумма товара: ${numItem * price}`);
+// console.log(nameItem);
+// console.log(`общая сумма товара: ${numItem * price}`);
 
 {
     const nameItem1 = 'Apple';
@@ -12,6 +12,23 @@ console.log(`общая сумма товара: ${numItem * price}`);
     const category1 = 'Fruits';
     let price1 = 10;
     
-    console.log(nameItem1);
-    console.log(`общая сумма товара: ${numItem1 * price1}`);
+    // console.log(nameItem1);
+    // console.log(`общая сумма товара: ${numItem1 * price1}`);
+}
+
+{
+    const itemNamePeompt = prompt('Напишите наименование товара');
+    console.log(typeof itemNamePeompt);
+    console.log(itemNamePeompt);
+    const itemNumPrompt = +prompt('Напишите количество товара');
+    console.log(typeof itemNumPrompt);
+    console.log(itemNumPrompt);
+    const itemCategoryPrompt = prompt('Напишите категорию товара');
+    console.log(typeof itemCategoryPrompt);
+    console.log(itemCategoryPrompt);
+    const itemtPricePrompt = +prompt('Напишите цену товара');
+    console.log(typeof itemtPricePrompt);
+    console.log(itemtPricePrompt);
+console.log(`На складе ${itemNumPrompt} единицы товара ${itemNamePeompt} на сумму ${itemtPricePrompt * itemNumPrompt} у.е.`);
+
 }
